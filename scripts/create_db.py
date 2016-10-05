b7f5f8c8d83c67f22102557d04443e55a2b07734
Doug Melvin <dmelvin@yourperfectcomputer.info>
2016-10-05 22:10:33 +0000

    Create a DB creation script

---
+++ scripts/create_db.py
@@ -0,0 +1,5 @@
+# Create a database here. 
+# Reference this article: http://stackoverflow.com/questions/19426448/creating-a-postgresql-db-using-psycopg2
+# Reference this tutorial: https://wiki.postgresql.org/wiki/Psycopg2_Tutorial
+# Reference this guide: https://www.postgresql.org/docs/9.2/static/libpq-pgpass.html
+# Consider refactoring later to do this: http://stackoverflow.com/questions/35153483/create-db-and-tables-from-setup-py
