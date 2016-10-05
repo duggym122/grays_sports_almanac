80733dc0ad251fdc271dde35dc99c2ed0bfad790
Doug Melvin <dmelvin@yourperfectcomputer.info>
2016-10-05 22:11:39 +0000

    Add tests file for database code

---
+++ tests/grays_almanac_database_tests.py
@@ -0,0 +1,12 @@
+from grays_sports_almanac import grays_hockey_database
+import psycopg2
+
+def test_database_existence():
+    pass
+
+def test_team_table_existence():
+    pass
+
+def test_team_columns():
+    pass
+    
