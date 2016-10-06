1219524aaca16c39d0637e19dea79014cd82214c
Doug Melvin <dmelvin@yourperfectcomputer.info>
2016-10-06 13:42:46 +0000

    working on database funtimes

--- grays_sports_almanac/grays_almanac_database/create_db.py
+++ grays_sports_almanac/grays_almanac_database/create_db.py
@@ -1,5 +1,19 @@
 # Create a database here. 
 # Reference this article: http://stackoverflow.com/questions/19426448/creating-a-postgresql-db-using-psycopg2
 # Reference this tutorial: https://wiki.postgresql.org/wiki/Psycopg2_Tutorial
-# Reference this guide: https://www.postgresql.org/docs/9.2/static/libpq-pgpass.html
 # Consider refactoring later to do this: http://stackoverflow.com/questions/35153483/create-db-and-tables-from-setup-py
+import passfile
+from psycopg2 import connect
+import sys
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+con = None
+con = connect(user='duggym122', host = 'localhost', port = '5432', password = passfile.dbpass)
+
+newdb = "grays_almanac"
+
+con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+cur = con.cursor()
+cur.execute('CREATE DATABASE ' + newdb)
+cur.close()
+con.close()
